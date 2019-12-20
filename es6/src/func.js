69c940a87c60fa79115a1ce8152d497b419ba122
NGowthamKumar <48080545+NGowthamKumar@users.noreply.github.com>
2019-12-20 11:53:41 +0530

    Delete func.js

--- es6/src/func.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-const schema = new mongoose.Schema({ name : String , id : Number , password : String });
-
-module.exports = mongoose.model('crud',schema);
