b909588c7267953bd6e12fc871ac2447d40308ad
gowthamkumar21011999@gmail.com <gowthamkumar21011999@gmail.com>
2020-03-12 19:22:12 +0530

    initial

--- es6/src/config/mongoose.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost:27017/mydb',{'useUnifiedTopology' : true , 'useNewUrlParser' : true },
-function(err){
-if(err) console.log("Can't connect MongoDB");
-else console.log("Connected to MongoDb...");
-});
-
-
--- es6/src/models/schema.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema; 
-const Mod = mongoose.Schema({ name : String , id : Number , password : String });
-module.exports = mongoose.model('crud',Mod);
-
